877d27813e54ee7ab5589acc6f73aff8b48098cc
leodipaula <leofernandes9@gnail.com>
2025-04-23 03:28:11 -0300

    Adicionado o BlackHound na main para detectar chamadas bloqueantes

--- src/main/java/leodepaula/com/loja_de_games/LojaDeGamesApplication.java
+++ src/main/java/leodepaula/com/loja_de_games/LojaDeGamesApplication.java
@@ -2,11 +2,17 @@ package leodepaula.com.loja_de_games;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 public class LojaDeGamesApplication {
 
+	static {
+		BlockHound.install();
+	}
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(LojaDeGamesApplication.class, args);
 	}
 
